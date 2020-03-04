7a7a82410a131c1a58ed8c7a59235c14097b7f42
xRadne <Alex.radne@gmail.com>
2020-03-04 22:29:29 +0100

    Fixed bugs, checks valid keys

--- NDSparseTensor.py
+++ NDSparseTensor.py
@@ -9,7 +9,7 @@ class NDSparseTensor:
     elif isinstance(shape, tuple):
       self.shape = shape
     else:
-      raise TypeError
+      raise TypeError(f"shape must be of type 'int' or 'tuple', not '{type(shape).__name__}'")
     self.dimension = len(self.shape)
 
   def __index_exists__(self, i, arr):
@@ -25,42 +25,42 @@ class NDSparseTensor:
       value *= v
     return value
 
-  def __sliceindicies__(self, slice):
-    return [x for x in range(*slice.indices(self.__numberofelements__()))]
+  def __sliceindicies__(self, slice, arraylength):
+    return [x for x in range(*slice.indices(arraylength))]
 
-  def __slicelength__(self, slice):
-    return len(self.__sliceindicies__(slice))
+  def __slicelength__(self, slice, arraylength):
+    return len(self.__sliceindicies__(slice, arraylength))
 
-  def __getslice__(self, slice):
-    r = range(*slice.indices(self.__numberofelements__()))
-    return [self.__getitem__(ii) for ii in r]
+  def __getslice__(self, slice, arraylength):
+    r = range(*slice.indices(arraylength))
+    return [self[ii] for ii in r]
 
   def __getitem__(self, key):
     if isinstance(key, slice):
-      return self.__getslice__(key)
+      return self.__getslice__(key, self.shape[0])
     elif isinstance(key, tuple):
-      if len(key) > self.dimension:
-        raise IndexError
+      if not len(key) == self.dimension:
+        raise KeyError(f"Dimensions must match. Given key '{key}' should be of length {self.dimension}")
       if all([isinstance(x, int) and x < self.shape[i] for i, x in enumerate(key)]):
         try:
           return self.elements[key]
         except KeyError:
           return 0
       shape = []
-      for k in key:
+      for dim, k in enumerate(key):
         if isinstance(k, int):
-          shape.append(k)
+          shape.append(1)
         elif isinstance(k, slice):
-          shape.append(self.__slicelength__(k))
+          shape.append(self.__slicelength__(k, arraylength=self.shape[dim]))
       shape = tuple(shape)
       submatrix = NDSparseTensor(shape)
       
       indicies = []
-      for k in key:
+      for dim, k in enumerate(key):
         if isinstance(k, int):
-          indicies.append(k)
+          indicies.append([k])
         elif isinstance(k, slice):
-          indicies.append(self.__sliceindicies__(k))
+          indicies.append(self.__sliceindicies__(k, arraylength=self.shape[dim]))
       
       stack = [0 for _ in range(len(key))]
       p = len(stack) - 1
@@ -80,16 +80,28 @@ class NDSparseTensor:
           stack[p + 1] = 0
           stack[p] += 1
         
-      return submatrix
-    else:
+      return submatrix.reducedimension()
+    elif isinstance(key, int):
       try:
-        value = self.elements[key]
+        value = self.elements[key,]
       except KeyError:
         # could also be 0.0 if using floats...
         value = 0
       return value
+    else:
+      raise TypeError(f"Key must be of type 'int', 'slice' or 'tuple' of intigers or slices, not '{type(key).__name__}'")
 
   def __setitem__(self, key, value):
+    if isinstance(key, int):
+      key = key,
+    if isinstance(key, tuple):
+      if not len(key) == self.dimension:
+         raise KeyError(f"Dimensions must match. Given key '{key}' should be of length {self.dimension}")
+      elif not all([k < self.shape[i] for i, k in enumerate(key)]):
+        raise IndexError(f"Index out of range. Key {key}, must be within {self.shape}")
+    else:
+      raise TypeError(f"Key must be of type 'int' or 'tuple' of intigers or slices, not '{type(key).__name__}'")
+
     if not value == 0:
       self.elements[key] = value
     else:
@@ -102,10 +114,10 @@ class NDSparseTensor:
     del self.elements[key]
 
   def __repr__(self):
-    return "NDSparseTensor()"
+    return f"NDSparseTensor({self.shape})"
 
   def __str__(self):
-    s = f'NDSparseTensor of size {self.shape}\n  '
+    s = self.__repr__()+'\n  '
     return s+'\n  '.join([str(x) for x in self.elements.items()])
 
   def getnonzero(self):
@@ -117,6 +129,19 @@ class NDSparseTensor:
       dense[key] = value
     return dense
 
+  def reducedimension(self):
+    old_shape = self.shape    
+    redundant_dimensions = [dim for dim, size in enumerate(old_shape) if size == 1]
+
+    new_shape = tuple([size for dim, size in enumerate(old_shape) if dim not in redundant_dimensions])
+
+    reduced = NDSparseTensor(new_shape)
+    for old_key, value in self.getnonzero():
+      new_key = tuple([x for dim, x in enumerate(old_key) if dim not in redundant_dimensions])
+      reduced[new_key] = self.elements[old_key]
+
+    return reduced
+
   def tojson(self):
     return json.dumps({
       "NDSparseTensor": {
@@ -126,7 +151,8 @@ class NDSparseTensor:
       }
     })
 
-  def fromjson(self, json_string):
+  @staticmethod
+  def fromjson(json_string):
     dictionary = json.loads(json_string)
     shape = tuple(dictionary["NDSparseTensor"]["shape"])
     elements = dictionary["NDSparseTensor"]["elements"]
@@ -135,7 +161,7 @@ class NDSparseTensor:
     for element in elements:
       for x in element.items():
         key, value = x
-        key = tuple(key.split(' '))
+        key = tuple([int(x) for x in key.split(' ')])
         matrix[key] = value
     return matrix
 
@@ -146,14 +172,13 @@ if __name__ == "__main__":
   matrix[1, 1] = 3
   matrix[1, 4] = 1
   matrix[2, 2] = 10
-  print(matrix.todense())
   
-
-  print(matrix[:3, :3].todense())
-
   json_string = matrix.tojson()
   print(json_string)
 
-  print(matrix.fromjson(json_string))
+  matrix2 = NDSparseTensor.fromjson(json_string)
+  print('matrix2:')
+  print(matrix2.todense())
 
-  matrix = matrix.fromjson(json_string)
+  print('Extract row:', matrix2[1, :].todense()) # Extract row
+  print('Extract column:', matrix2[:, 1].todense()) # Extract column
